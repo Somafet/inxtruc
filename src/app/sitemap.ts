ce2b19ec77b962e133311731b8b677a721a14cf0
Soma Somorjai <soma@dinge.hu>
2025-01-17 15:57:51 +0100

    Update sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,5 +6,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: 'https://inxtruc.com',
       priority: 1,
     },
+    {
+      url: 'https://inxtruc.com/airbnb',
+      priority: 0.9,
+    },
+    {
+      url: 'https://inxtruc.com/booking',
+      priority: 0.9,
+    },
+    {
+      url: 'https://inxtruc.com/families',
+      priority: 0.9,
+    },
   ]
 }
